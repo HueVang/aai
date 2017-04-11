f3be481f7f20dbec05230f244beb28af4a240957
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-04-11 01:41:51 -0500

    Added POST video functionality.

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -1,11 +1,11 @@
 angular.module('aai').controller('HomeController', function($http, $location) {
-  var showLogs = true; 
+  var showLogs = true;
 
   var ctrl = this;
   ctrl.videos = ['test', 'test2', 'test3'];
 
   ctrl.working = function() {
-    console.log('HomeController loaded');
+    if (showLogs) console.log('HomeController loaded');
   }; // end ctrl.working
 
   ctrl.working();
@@ -20,10 +20,25 @@ angular.module('aai').controller('HomeController', function($http, $location) {
 
   ctrl.getAllVideos = function() {
     $http.get('https://proofapi.herokuapp.com/videos?1&10').then(function(res){
-      ctrl.videos = res.data;
+      ctrl.videos = res.data.data;
       if (showLogs) console.log(res);
     });
   }; // end ctrl.getAllVideos
+
+  ctrl.slug = function(title) {
+    ctrl.video.slug = title.toString().toLowerCase()
+      .replace(/\s+/g, '-')
+      .replace(/^-+/, '')
+      .replace(/-+$/, '');
+  }; // end ctrl.slug
+
+  ctrl.showVideo = function() {
+    ctrl.slug(ctrl.video.title)
+    var video = ctrl.video
+    $http.post('https://proofapi.herokuapp.com/videos', video).then(function(res){
+      if (showLogs) console.log('The response:', res);
+    })
+  }; // end ctrl.showVideo
 
 
 }); // end angular.module
