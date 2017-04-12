b9ac15457f87d5507bf559cc13257d6a057adf59
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-04-12 05:18:24 -0500

    Added link to watch videos. Added view POST when video is watched.

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -2,13 +2,13 @@ angular.module('aai').controller('HomeController', function($http, $location) {
   var showLogs = true; // set to true to show console logs, false to hide.
 
   var ctrl = this;
-  ctrl.videos = [];
 
   ctrl.working = function() {
     if (showLogs) console.log('HomeController loaded');
   }; // end ctrl.working
-
   ctrl.working();
+
+  ctrl.videos = [];
 
   ctrl.getAllVideos = function() {
     $http.get('https://proofapi.herokuapp.com/videos?1&10').then(function(res){
@@ -27,7 +27,6 @@ angular.module('aai').controller('HomeController', function($http, $location) {
       ctrl.getAllVideos();
     });
   }; // end ctrl.setAuthToken
-
   ctrl.setAuthToken();
 
   ctrl.slug = function(title) {
@@ -42,9 +41,19 @@ angular.module('aai').controller('HomeController', function($http, $location) {
     var video = ctrl.video
     $http.post('https://proofapi.herokuapp.com/videos', video).then(function(res){
       if (showLogs) console.log('The response:', res);
+      ctrl.getAllVideos();
     })
     ctrl.video = {};
-    ctrl.getAllVideos();
   }; // end ctrl.showVideo
+
+
+  ctrl.watchVideo = function(videoId) {
+    var video = {'video_id' : videoId};
+    if (showLogs) console.log('This is the video object:', video);
+    $http.post('https://proofapi.herokuapp.com/views', video).then(function(res){
+      if (showLogs) console.log('This is the response:', res);
+    })
+
+  }; // end ctrl.watchVideo
 
 }); // end angular.module
