bd3740b9e3ca72d963f65944c636bc520f4e1a0d
Hue  Vang <huevang@Hues-MacBook-Pro.local>
2017-04-12 04:01:04 -0500

    Added feature that sorts videos by most recent date. Also added clear input feature on video submission.

--- public/scripts/home.controller.js
+++ public/scripts/home.controller.js
@@ -1,8 +1,8 @@
 angular.module('aai').controller('HomeController', function($http, $location) {
-  var showLogs = true;
+  var showLogs = true; // set to true to show console logs, false to hide.
 
   var ctrl = this;
-  ctrl.videos = ['test', 'test2', 'test3'];
+  ctrl.videos = [];
 
   ctrl.working = function() {
     if (showLogs) console.log('HomeController loaded');
@@ -20,7 +20,11 @@ angular.module('aai').controller('HomeController', function($http, $location) {
 
   ctrl.getAllVideos = function() {
     $http.get('https://proofapi.herokuapp.com/videos?1&10').then(function(res){
-      ctrl.videos = res.data.data;
+      ctrl.videos = res.data.data.sort(function(a, b){
+        a = new Date(a.attributes.created_at);
+        b = new Date(b.attributes.created_at);
+        return a > b ? -1 : a < b ? 1 : 0;
+      });
       if (showLogs) console.log(res);
     });
   }; // end ctrl.getAllVideos
@@ -32,13 +36,14 @@ angular.module('aai').controller('HomeController', function($http, $location) {
       .replace(/-+$/, '');
   }; // end ctrl.slug
 
-  ctrl.showVideo = function() {
+  ctrl.addVideo = function() {
     ctrl.slug(ctrl.video.title)
     var video = ctrl.video
     $http.post('https://proofapi.herokuapp.com/videos', video).then(function(res){
       if (showLogs) console.log('The response:', res);
     })
+    ctrl.video = {};
+    ctrl.getAllVideos();
   }; // end ctrl.showVideo
-
 
 }); // end angular.module
